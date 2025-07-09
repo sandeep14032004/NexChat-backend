7a15f8d92ae10638d8a394a2a9f139ed2e92c066
sandeep14032004 <sandeeppati69@gmail.com>
2025-07-09 10:46:54 +0530

    this 1

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userid, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     secure: process.env.NODE_ENV !== "development",
   });
   return token;
