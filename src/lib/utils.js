1d3c966f4a8e32d8f852153c1ecd7cfcfd602ce3
sandeep14032004 <sandeeppati69@gmail.com>
2025-07-09 09:48:59 +0530

    fixing token issue

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userid, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     secure: process.env.NODE_ENV !== "development",
   });
   return token;
